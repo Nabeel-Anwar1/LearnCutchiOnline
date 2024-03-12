8a62183623ac47add5957807f80ffb816058cc03
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2024-03-12 11:37:13 +0000

    added in routers and start of the home and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./components/Home/HomePage";
+import LoginPage from "./components/Login/LoginPage";
 
 function App() {
-
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
