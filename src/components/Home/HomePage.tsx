397aa12e231a2a9525ef42c4d22507428c65e096
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2024-03-13 18:37:00 +0000

    home page message

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <Link to="/Login">Login!</Link>;
+  return (
+    <div>
+      <Link to="/Login">Login!</Link>
+      <p>Are you ready to learn the language Cutchi?</p>
+      <p>
+        You can learn by listening to some recordings of native speakers or by
+        playing some games!
+      </p>
+      <p>Sign up now to track your progress and learn!</p>
+    </div>
+  );
 };
 
 export default HomePage;
